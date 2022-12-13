dad18605d5bb84d3bff2e7a8e070b4387bec2e79
Paul <pavel.zarubin@metalamp.io>
2022-12-13 14:35:44 +0300

    add base functions

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use rust_telegram_radio::*;
+use std::env;
+use std::fs;
+use rand::Rng;
+
+
+fn main() {
+    
+    let args: Vec<String> = env::args().collect();
+    let token = &args[1];
+    let chat_id = args[2].parse::<i64>().unwrap();
+    let paths: Vec<Result<fs::DirEntry, std::io::Error>> = fs::read_dir("./music_lib").unwrap().collect();
+
+    let nums = paths.len();
+    let mut rng = rand::thread_rng();
+
+    loop {
+        let i = rng.gen_range(0..nums);
+        let next_track_path = format!("{}",paths[i].as_ref().unwrap().path().display());
+        let next_track = read_info(&next_track_path);
+        send_message(token,chat_id,&next_track);
+        play_file(&next_track_path)
+    }
+    
+}
